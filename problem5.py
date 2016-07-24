8dee4ad5dc32dc3b4f5627a28b90c6a1dbe89dab
Tashay Green <tashay.g@gmail.com>
2016-07-24 13:58:51 -0400

    Problem 5 with While Loop.

--- problem5.py
+++ problem5.py
@@ -1,14 +1,19 @@
 import re
 
+def check_password(password):
+    lower = re.search("[a-z]", password)
+    upper = re.search("[A-Z]", password)
+    digit = re.search("[0-9]", password)
+    char = re.search("[$@#]", password)
+    length = len(password)>=6 and len(password)<=12
+
+    if lower and upper and digit and char and length:
+        print('Strong password.')
+    else:
+        print('Weak password.')
+
 password = input('Please enter your password.\n')
 
-lower = re.search("[a-z]", password)
-upper = re.search("[A-Z]", password)
-digit = re.search("[0-9]", password)
-char = re.search("[$@#]", password)
-length = len(password)>=6 and len(password)<=12
-
-if lower and upper and digit and char and length:
-    print('Strong password.')
-else:
-    print('Weak password.')
+while (password != ''):
+    check_password(password)
+    password = input('Please enter your password.\n')
