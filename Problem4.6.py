bea73d415a28f19dcd9f18d7e92c9fad893a7bf3
Tashay Green <tashay.g@gmail.com>
2016-08-27 11:05:51 -0400

    Bayes probability theorem

---
+++ Problem4.6.py
@@ -0,0 +1,18 @@
+#A)
+pA = 0.06
+pB = 0.05
+pAifB = 0.6
+pBifA = ((pAifB * pB)/pA)
+
+pAandB = pA * pBifA
+
+print(pAandB)
+
+#B)
+pA = 0.06
+pB = 0.05
+pAifB = 0.6
+
+pBifA = ((pAifB * pB)/pA)
+
+print(pBifA)
